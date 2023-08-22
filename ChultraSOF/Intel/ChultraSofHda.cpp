a450aa57e9067aa4ebc6227ee39f9412ec20deac
Avery Black <avery.c.black@gmail.com>
2023-08-21 22:49:07 -0700

    Add firmware requests from kext bundle

--- ChultraSOF/Intel/ChultraSofHda.cpp
+++ ChultraSOF/Intel/ChultraSofHda.cpp
@@ -7,6 +7,8 @@
 
 #include "ChultraSofHda.hpp"
 #include <IOKit/IOLib.h>
+#include <IOKit/IOLocks.h>
+#include <libkern/OSKextLib.h>
 #include <Availability.h>
 
 #ifndef __ACIDANTHERA_MAC_SDK
@@ -16,8 +18,30 @@
 #define super IOService
 OSDefineMetaClassAndStructors(ChultraSofHda, IOService);
 
+struct ChultraResourceLoadContext {
+    IOLock *lock;
+    OSData *resource;
+};
+
+static void ChultraSofOnFirmwareCallback(
+    OSKextRequestTag requestTag,
+    OSReturn result,
+    const void *resourceData,
+    uint32_t resourceDataLength,
+    void *context) {
+    
+    ChultraResourceLoadContext *ctx = (ChultraResourceLoadContext *) context;
+    IOLockLock(ctx->lock);
+    if (result == kOSReturnSuccess) {
+        ctx->resource = OSData::withBytes(resourceData, resourceDataLength);
+    }
+    IOLockUnlock(ctx->lock);
+    IOLockWakeup(ctx->lock, ctx, false);
+}
+
 ChultraSofHda *ChultraSofHda::probe(IOService *provider, SInt32 *score) {
     OSObject *topologyResult;
+    ChultraResourceLoadContext ctx {0};
     
     if (super::probe(provider, score) == nullptr) {
         IOLog("SOF::Failed to probe\n");
@@ -35,7 +59,10 @@ ChultraSofHda *ChultraSofHda::probe(IOService *provider, SInt32 *score) {
     pci->setBusMasterEnable(true);
     pci->setMemoryEnable(true);
     
-    // Grab Topology from
+    //
+    // Grab topology
+    //
+    
     OSString *acpiDevPath = OSDynamicCast(OSString, pci->getProperty("acpi-path"));
     if (acpiDevPath == nullptr) {
         IOLog("SOF::Failed to grab ACPI Device\n");
@@ -53,12 +80,40 @@ ChultraSofHda *ChultraSofHda::probe(IOService *provider, SInt32 *score) {
     IOReturn ret = acpiDev->evaluateObject("_DSD", &topologyResult);
     if (ret != kIOReturnSuccess) {
         IOLog("SOF::Failed to grab topology\n");
-        OSSafeReleaseNULL(acpiDev);
-        return nullptr;
+        goto err;
     }
     
+    //
+    // Load Firmware from kext bundle
+    //
+    
+    ctx.lock = IOLockAlloc();
+    ctx.resource = nullptr;
+    if (ctx.lock == nullptr) {
+        ret = kIOReturnError;
+        goto err;
+    }
+    
+    IOLockLock(ctx.lock);
+    ret = OSKextRequestResource(OSKextGetCurrentIdentifier(), "firmware name", ChultraSofOnFirmwareCallback, &ctx, nullptr);
+    if (ret != kIOReturnSuccess) {
+        IOLog("SOF::Failed to request firmware\n");
+        goto err;
+    }
+    
+    ret = IOLockSleep(ctx.lock, &ctx, false);
+    if (ret != kIOReturnSuccess || ctx.resource == nullptr) {
+        IOLog("SOF::Failed to grab firmware\n");
+        ret = kIOReturnError;
+        goto err;
+    }
+    
+    IOLockUnlock(ctx.lock);
+err:
+    if (ctx.lock != nullptr) IOLockFree(ctx.lock);
+    OSSafeReleaseNULL(ctx.resource);
     OSSafeReleaseNULL(acpiDev);
-    return this;
+    return ret != kIOReturnSuccess ? nullptr : this;
 }
 
 bool ChultraSofHda::start(IOService *provider) {
