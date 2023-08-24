0c6394b1cdc1647abca48f0aee50fee6ab62f270
Avery Black <avery.c.black@gmail.com>
2023-08-23 19:42:27 -0700

    Start parsing streams + caps

--- ChultraSOF/Intel/ChultraSofHda.cpp
+++ ChultraSOF/Intel/ChultraSofHda.cpp
@@ -7,6 +7,7 @@
 
 #include "ChultraSofHda.hpp"
 #include "Logger.h"
+#include <IOKit/IOFilterInterruptEventSource.h>
 #include <IOKit/IOLib.h>
 #include <IOKit/IOLocks.h>
 #include <libkern/OSKextLib.h>
@@ -85,12 +86,14 @@ LIBKERN_RETURNS_RETAINED OSData *ChultraSofHda::getFirmware(const char *firmware
     return ctx.resource;
 }
 
+bool ChultraSofHda::init() {
+    bzero(barMaps, sizeof(barMaps));
+    return true;
+}
+
 ChultraSofHda *ChultraSofHda::probe(IOService *provider, SInt32 *score) {
     OSObject *topologyResult;
     OSData *firmware = nullptr;
-    IOMemoryMap *dspBar = nullptr;
-    IOMemoryMap *hdaBar = nullptr;
-    int interruptIndex;
     
     IOLogInfo("Driver startup!");
     
@@ -140,29 +143,105 @@ ChultraSofHda *ChultraSofHda::probe(IOService *provider, SInt32 *score) {
     
     firmware = getFirmware("firmware name");
     
-    dspBar = pci->mapDeviceMemoryWithRegister(kIOPCIConfigBaseAddress4);
-    hdaBar = pci->mapDeviceMemoryWithRegister(kIOPCIConfigBaseAddress0);
+    barMaps[SOF_DSP_BAR] = pci->mapDeviceMemoryWithRegister(kIOPCIConfigBaseAddress4);
+    barMaps[SOF_HDA_BAR] = pci->mapDeviceMemoryWithRegister(kIOPCIConfigBaseAddress0);
     
-    if (dspBar != nullptr) {
-        IOLogInfo("DSP Bar Addr: 0x%llx Length: 0x%llx", dspBar->getPhysicalAddress(), dspBar->getSize());
+    if (barMaps[SOF_DSP_BAR] != nullptr) {
+        IOLogInfo("DSP Bar Addr: 0x%llx Length: 0x%llx", barMaps[SOF_DSP_BAR]->getPhysicalAddress(), barMaps[SOF_DSP_BAR]->getSize());
     }
     
-    if (hdaBar != nullptr) {
-        IOLogInfo("HDA Bar Addr: 0x%llx Length: 0x%llx", hdaBar->getPhysicalAddress(), hdaBar->getSize());
+    if (barMaps[SOF_HDA_BAR] != nullptr) {
+        IOLogInfo("HDA Bar Addr: 0x%llx Length: 0x%llx", barMaps[SOF_HDA_BAR]->getPhysicalAddress(), barMaps[SOF_HDA_BAR]->getSize());
     }
     
-    interruptIndex = getMsiInterruptIndex();
-    IOLogInfo("Found MSI interrupt index %d", interruptIndex);
+    ctrlGetCaps();
+    getHdaStreams();
     
 err:
-    OSSafeReleaseNULL(dspBar);
-    OSSafeReleaseNULL(hdaBar);
     OSSafeReleaseNULL(firmware);
     OSSafeReleaseNULL(acpiDev);
     return ret != kIOReturnSuccess ? nullptr : this;
 }
 
 bool ChultraSofHda::start(IOService *provider) {
+    int interruptIndex;
+    
+    // TODO: Register Interrupts
+    interruptIndex = getMsiInterruptIndex();
+    IOLogInfo("Found MSI interrupt index %d", interruptIndex);
+    
+    workloop = IOWorkLoop::workLoop();
+    cmdGate = IOCommandGate::commandGate(this);
+    intSrc = IOFilterInterruptEventSource::filterInterruptEventSource(this, nullptr, nullptr, provider);
+    if (intSrc == nullptr || workloop == nullptr || cmdGate == nullptr) {
+        IOLogError("Failed to construct interrupt sources");
+        return false;
+    }
+    
+    if (workloop->addEventSource(cmdGate) != kIOReturnSuccess ||
+        workloop->addEventSource(intSrc) != kIOReturnSuccess) {
+        IOLogError("Failed to add event sources to workloop");
+        return false;
+    }
+    
     registerService();
     return super::start(provider);
+}
+
+void ChultraSofHda::free() {
+    for (size_t i = 0; i < SOF_MAX_BAR; i++) {
+        OSSafeReleaseNULL(barMaps[i]);
+    }
+}
+
+IOReturn ChultraSofHda::ctrlGetCaps() {
+    // TODO: Throw reset in it's own method and return reset errors
+    // Enter reset
+    uint32_t ctrlReg = read32(SOF_HDA_BAR, 0x8);
+    ctrlReg &= ~0x1;
+    write32(SOF_HDA_BAR, 0x8, ctrlReg);
+    IOSleep(100);
+    
+    ctrlReg = read32(SOF_HDA_BAR, 0x8);
+    IOLogInfo("Ctrl Reg = 0x%x", ctrlReg);
+    
+    // Exit reset
+    ctrlReg |= ~0x1;
+    write32(SOF_HDA_BAR, 0x8, ctrlReg);
+    IOSleep(100);
+    IOLogInfo("Ctrl Reg = 0x%x", ctrlReg);
+    
+    uint32_t offset = read32(SOF_HDA_BAR, 0x14);
+    int count = 0;
+    
+    do {
+        uint32_t capability = read32(SOF_HDA_BAR, offset);
+        uint32_t feature = (capability & 0x7FF0000) >> 16;
+        IOLogInfo("Found capability %d at 0x%x", feature, offset);
+        switch (feature) {
+            case SOF_HDA_PP_CAP_ID: bars[SOF_PP_BAR] = bars[SOF_HDA_BAR] + offset; break;
+            case SOF_HDA_SPIB_CAP_ID: bars[SOF_SPIB_BAR] = bars[SOF_SPIB_BAR] + offset; break;
+            case SOF_HDA_DRSM_CAP_ID: bars[SOF_DRSM_BAR] = bars[SOF_DRSM_BAR] + offset; break;
+            case SOF_HDA_GTS_CAP_ID: break;
+            case SOF_HDA_ML_CAP_ID: break;
+            default: break;
+        }
+        offset = capability & 0xFFFF;
+        count++;
+    } while (count <= 10 && offset != 0);
+    
+    return kIOReturnSuccess;
+}
+
+IOReturn ChultraSofHda::getHdaStreams() {
+    uint32_t streamCaps = read32(SOF_HDA_BAR, 0x0);
+    
+    int captureStreams = (streamCaps >> 8) & 0x0F;
+    int playbackStreams = (streamCaps>>12) & 0x0F;
+//    int totalStream = captureStreams + playbackStreams;
+    IOLogInfo("Found %d playback and %d capture streams", playbackStreams, captureStreams);
+    
+    // TODO: Actually parse/create streams
+    
+    return kIOReturnSuccess;
 }
--- ChultraSOF/Intel/ChultraSofHda.hpp
+++ ChultraSOF/Intel/ChultraSofHda.hpp
@@ -9,19 +9,59 @@
 #define ChultraSofHda_hpp
 
 #include <IOKit/IOService.h>
+#include <IOKit/IOFilterInterruptEventSource.h>
+#include <IOKit/IOCommandGate.h>
 #include <IOKit/pci/IOPCIDevice.h>
 #include <IOKit/acpi/IOACPIPlatformDevice.h>
+
+enum hda_cap_ids_t {
+    SOF_HDA_GTS_CAP_ID = 1,
+    SOF_HDA_ML_CAP_ID = 2,
+    SOF_HDA_PP_CAP_ID = 3,
+    SOF_HDA_SPIB_CAP_ID = 4,
+    SOF_HDA_DRSM_CAP_ID = 5
+};
+
+enum hda_bars_t {
+    SOF_HDA_BAR,
+    SOF_PP_BAR,
+    SOF_SPIB_BAR,
+    SOF_DRSM_BAR,
+    SOF_DSP_BAR,
+    SOF_MAX_BAR
+};
 
 class ChultraSofHda : public IOService {
     OSDeclareDefaultStructors(ChultraSofHda);
   
+    bool init() override;
     bool start(IOService *provider) override;
+    void free() override;
     ChultraSofHda *probe(IOService *provider, SInt32 *score) override;
     
     int getMsiInterruptIndex();
     OSData *getFirmware(const char *firmwareName);
 protected:
     IOPCIDevice *pci {nullptr};
+    
+    inline uint32_t read32(hda_bars_t bar, IOVirtualAddress offset) {
+        return *(uint32_t *)((uint8_t *) bars[bar] + offset);
+    }
+    
+    inline void write32(hda_bars_t bar, IOVirtualAddress offset, uint32_t newVal) {
+        *(uint32_t *)((uint8_t *) bars[bar] + offset) = newVal;
+    }
+    
+    IOMemoryMap *barMaps[SOF_MAX_BAR];
+    IOVirtualAddress bars[SOF_MAX_BAR];
+    
+    IOWorkLoop *workloop {nullptr};
+    IOFilterInterruptEventSource *intSrc {nullptr};
+    IOCommandGate *cmdGate {nullptr};
+
+private:
+    IOReturn ctrlGetCaps();
+    IOReturn getHdaStreams();
 };
 
 
